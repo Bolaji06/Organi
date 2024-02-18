509a5f7e118f1e7dae330977d8ff82e60848597e
Bolaji06 <joshspencer0603@gmail.com>
2024-02-18 21:24:56 +0100

    changes

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -10,31 +10,6 @@ export const nextAuthOptions: NextAuthOptions = {
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
         }),
-        // CredentialsProvider({
-        //     name: "Credentials",
-        //     credentials: {
-        //         username: {
-        //             label: "Username",
-        //             type: "text",
-        //             placeholder: "Enter your username",
-                    
-        //         },
-        //         password: {
-        //             label: "Password",
-        //             type: "password",
-        //             placeholder: "Enter your password",
-                    
-        //         }
-        //     },
-        //     async authorize(credentials){
-        //         const user = { id: "11", name: "Bolaji", password: "password" };
-
-        //         if (credentials?.username === user.name && 
-        //             credentials?.password === user.password){
-        //                 return user;
-        //             } else { return null; }
-        //     }
-        // })
     ],
     pages: {
         signIn: '/signin',
